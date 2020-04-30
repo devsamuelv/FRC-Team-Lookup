8c5858049097441f157a06409c6f530052c3b65f
DevSamuelV <realalloutmusic@gmail.com>
2020-04-30 16:06:21 -0400

    ✔ added support for data that doesent exist ✔

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from util import util
 from util import vars
 
@@ -10,15 +11,22 @@ driver_options = Options()
 
 # headless turns off the window
 driver_options.add_argument("--headless")
-#driver_options.add_argument("--disable-extensions")
-#driver_options.add_argument("--disable-gpu")
-#driver_options.add_argument("--no-sandbox") # linux only
+driver_options.add_argument("--disable-extensions")
+driver_options.add_argument("--disable-gpu")
+# driver_options.add_argument("--no-sandbox") # linux only
 
 # start the driver and add options
 driver = webdriver.Firefox(options=driver_options)
 
 teamNumber = input("\033[33m" + "Input FRC Team Number >> ")
 
+# checks if the element exists
+def CheckExists(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
 
 def main():
     CurrentURL = vars.defaultURL + teamNumber
@@ -34,17 +42,40 @@ def main():
     if driver.current_url != CurrentURL:
         return
 
-    info = driver.find_element_by_xpath(vars.teamInfoXPath).text
-    From = driver.find_element_by_xpath(vars.teamFromXPath).text
-    website = driver.find_element_by_xpath(vars.teamWebsiteXPath).text
-    name = driver.find_element_by_xpath(vars.teamNameXPath).text
-    robotName = driver.find_element_by_xpath(vars.robotNameXPath).text
+    info = ""
+    From = ""
+    website = ""
+    name = ""
+    robotName = ""
+
+    if CheckExists(vars.teamInfoXPath) == False:
+        info = "None"
+    else:
+        info = driver.find_element_by_xpath(vars.teamInfoXPath).text
+
+    if CheckExists(vars.teamFromXPath) == False:
+        From = "None"
+    else:
+        From = driver.find_element_by_xpath(vars.teamFromXPath).text
+
+    if CheckExists(vars.teamWebsiteXPath) == False:
+        website = "None"
+    else:
+        website = driver.find_element_by_xpath(vars.teamWebsiteXPath).text
+
+    if CheckExists(vars.teamNameXPath) == False:
+        name = "None"
+    else:
+        name = driver.find_element_by_xpath(vars.teamNameXPath).text
+
+    if CheckExists(vars.robotNameXPath) == False:
+        robotName = "None"
+    else:
+        robotName = driver.find_element_by_xpath(vars.robotNameXPath).text
 
     util.PrintPageData(name, From, robotName, website, info)
 
+    driver.close()
+
 
 main()
-
-# print the current address
-def PrintCurrentAddr():
-    print(driver.current_url)
--- README.md
+++ README.md
@@ -5,6 +5,7 @@
     - pip
     - python3
     - firefox
+    
 - Steps
     - 1 make sure pip is in your path
     - 2 install selenium using pip `pip install selenium`
