0901c2e2d92b329d056f432896f7a87eab567156
DevSamuelV <realalloutmusic@gmail.com>
2020-07-22 14:39:01 -0400

    updated setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,26 @@
+curl -o geckodriver.zip https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-win64.zip
+
+SET localDir=%CD%
+
+@echo off
+setlocal
+cd /d %~dp0
+Call :UnZipFile localDir  localDir+"\geckodriver.zip"
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
+
 pip3 install selenium
